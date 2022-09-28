eb5e4490570eb4172debf6cd4cecfea73b399a29
ashish6564 <singhashish6564@gmail.com>
2022-09-28 11:40:25 +0530

    const is added in user,js

--- src/User.js
+++ src/User.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const User=()=> {
+const User=(props)=> {
+   
+    const {data}=props
   return (
     <div>
       <h1>User component</h1>
+      <h2>{data.name}</h2>
+      <h2>{data.age}</h2>
     </div>
   )
 }
