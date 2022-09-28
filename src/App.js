bc56b676d555d1f064d9cc78762fd17a601a4dcb
ashish6564 <singhashish6564@gmail.com>
2022-09-28 11:32:51 +0530

    component added in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import User from './User'
 
 function App() {
   return (
     <div className="App">
       <h1>hello dear </h1>
+      <User />
     </div>
   );
 }
